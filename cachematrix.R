35559ddd7a8cddc894d2c19ed30344ee81b58fdb
Armando Guereca <aguereca@gmail.com>
2015-03-21 23:31:22 -0700

    Assignment solution

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+#
+# Test case:
+#
+# # Test matrix
+# origin <- matrix(c(2,4,7,1,6,2,9,7,6),3,3)
+# source <- makeCacheMatrix(origin)
+# # Initial operation, no cache
+# cacheSolve(source)
+# # use cache on subsecuent attepts
+# cacheSolve(source)
+#
 
-## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    # cache Matrix with empty inverse
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
-
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    # prove cache
+    if(!is.null(m)) {
+        message("Getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    # warm cache of inverse
+    x$setsolve(m)
+    print(m)
 }
+
+
